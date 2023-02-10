b9b9dfefb366f69d6dde5fa0279cec26460fbe24
kfbastidas <kevithchi@unicauca.edu.co>
2023-02-10 12:56:55 -0500

    Update cursos.component.ts

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -17,6 +17,8 @@ export class CursosComponent implements OnInit{
       this.cursos = cursos;
       if (cursos.length==0) {
         swal.fire('Información', `La lista de cursos esta vacia`, 'info');  
+
+        
       }
     });
   }
@@ -32,16 +34,62 @@ export class CursosComponent implements OnInit{
         console.log(result);
         var curso:Curso = result;
         var mensaje = "***Información del Curso***<br>";
-        mensaje += "Id Curso: " + result.idCurso + "<br>";
-        mensaje += "Nombre Curso: " + result.nombre + "";
-        mensaje += "Periodo: " + result.periodo + "";
-        mensaje += "Nombre Curso: " + result.nombre + "<br>";
-        mensaje += "***Información de la Asignatura***<br>";
-        mensaje += "Id Asignatura: " + result.objAsignatura.idAsignatura + "<br>";
-        mensaje += "Nombre Curso: " + result.nombre + "<br>";
+        mensaje += "<b>Id Curso: </b>" + result.idCurso + " &nbsp;&nbsp;";
+        mensaje += "<b>Nombre Curso: </b>" + result.nombre + " &nbsp;&nbsp;";
+        mensaje += "<b>Periodo: </b>" + result.periodo + " <br>";
+        mensaje += "<br>***Información de la Asignatura***<br>";
+        mensaje += "<b>Id Asignatura: </b>" + result.objAsignatura.idAsignatura + "&nbsp;&nbsp;";
+        mensaje += "<b>Asignatura: </b>" + result.objAsignatura.nombre + "&nbsp;&nbsp;";
+        if(result.objAsignatura.docentes.length>0){
+          mensaje += "<br><br>***Información de los docentes***<br>";
+          for (const docente of curso.objAsignatura.docentes) {
+            mensaje += "<b>Id: </b>" + docente.idPersona + " &nbsp;&nbsp;";
+            mensaje += "<b>Nombres: </b>" + docente.nombres + " &nbsp;&nbsp;";
+            mensaje += "<b>Apellidos: </b>" + docente.apellidos + " &nbsp;&nbsp;";
+            mensaje += "<b>Tipo Identificación: </b>" + docente.tipoIdentificacion + " &nbsp;&nbsp;";
+            mensaje += "<b>No. Identificación: </b>" + docente.noIdentificacion + " &nbsp;&nbsp;";
+            mensaje += "<b>Universidad: </b>" + docente.universidad + " &nbsp;&nbsp;";
+            mensaje += "<b>Tipo Docente: </b>" + docente.tipoDocente + " &nbsp;&nbsp;";
+            mensaje += "<b>Salario: </b>" + docente.salario + " &nbsp;&nbsp;<br><br>";
+          }
+        }
+        
+        
 
-
-        swal.fire('Resultado',mensaje);
+        swal.fire({title:'Resultado',html:mensaje,width:1200});
+        /*
+        swal.fire({
+          title: '<strong>HTML <u>example</u></strong>',
+          icon: 'info',
+          width: 1000,
+          html:
+            `<table class="table table-bordered table-striped mt-2" *ngIf="cursos.length>0">
+            <thead>
+                <tr>
+                    <th>id curso</th>
+                    <th>nombre curso</th>
+                    <th>periodo curso</th>
+                    <th>id asignatura</th>
+                    <th>nombre asignatura</th>
+                </tr>
+            </thead>
+            <tbody>
+                <td>${result.idCurso}</td>
+                <td>{{curso.nombre}}</td>
+                <td>{{curso.periodo}}</td>
+                <td>{{curso.objAsignatura.idAsignatura}}</td>
+                <td>{{curso.objAsignatura.nombre}}</td>
+                <tr *ngFor="let curso of ${this.cursos}">
+                    <td>{{curso.idCurso}}</td>
+                    <td>{{curso.nombre}}</td>
+                    <td>{{curso.periodo}}</td>
+                    <td>{{curso.objAsignatura.idAsignatura}}</td>
+                    <td>{{curso.objAsignatura.nombre}}</td>
+                </tr>
+            </tbody>
+        </table>`
+        });
+        */
       },
       (error)=>{
           if (error.status==404) {
